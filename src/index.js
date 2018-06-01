04f78a8971ae54d599202dd4e820a68ab83a30a4
Gabrielle Saman <gabi.duarte@gmail.com>
2018-06-01 13:31:51 -0300

    Initial Commit
    - Setup structure for App

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './sass/style.scss';
+
+const Index = () => (
+    <h1>Gupy</h1>
+)
+
+ReactDOM.render(<Index/>, document.getElementById('root'));
