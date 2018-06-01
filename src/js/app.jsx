d3be13159c0506b16cdb15b5895c636214b72fc8
Gabrielle Saman <gabi.duarte@gmail.com>
2018-06-01 16:18:43 -0300

    Create page header

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -3,12 +3,12 @@ import React from 'react';
 import './../sass/style.scss';
 
 import Routes from './routes';
+import Header from './template/header';
 
 
 export default props => (
     <main className="container">
-        <h1>App</h1>
-
+        <Header/>
         <Routes/>
     </main>
 )
---
+++ src/js/template/header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default props => (
+    <header className="header">
+        <ul>
+            <li><a href="/">View Resumes</a></li>
+            <li><a href="/candidate">Add new candidate</a></li>
+        </ul>
+    </header>
+)
