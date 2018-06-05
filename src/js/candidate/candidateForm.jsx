839cd0f82b5f3ef26c8a7f0cac162a7aa7e39834
Gabrielle Saman <gabi.duarte@gmail.com>
2018-06-05 18:41:46 -0300

    Add validation to Candidate Form

--- src/js/candidate/candidateForm.jsx
+++ src/js/candidate/candidateForm.jsx
@@ -1,76 +1,94 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 
-const CandidateForm = props => (
-    <div> 
-        <h2> Add a new Candidate </h2>
-        <form onSubmit={props.handleSubmit}>
-            <div>
-                <label htmlFor="Name">Full name</label>
-                <Field name="name" component="input" type="text" />
-            </div>
-            <div>
-                <label htmlFor="email">Email</label>
-                <Field name="email" component="input" type="email" />
-            </div>
+const validate = values => {
+    const errors = {};
 
-            <div>
-                <label htmlFor="picture">picture</label>
-                <Field name="picture" component="input" type="text" />
-            </div>
+    ['name', 'phone', 'email', 'address'].forEach((field) => {
+        if (!values[field]) errors[field] = 'Required';
+    })
+    
+    if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = 'Invalid e-mail address';
+    }
 
-            <div>
-                <label htmlFor="birthDate">Birth Date</label>
-                <Field name="birthDate" component="input" type="date" />
-            </div>
+    return errors;
+}
 
-            <div>
-                <label htmlFor="gender">Gender</label>
-                <label><Field name="gender" component="input" type="radio" value="male"/> Male</label>
-                <label><Field name="gender" component="input" type="radio" value="female"/> Female</label>
-            </div>
-
-            <div>
-                <label htmlFor="phone">Phone</label>
-                <Field name="phone" component="input" type="number" />
-            </div>
-
-            <div>
-                <label htmlFor="address">Address</label>
-                <Field name="address" component="input" type="text" />
-            </div>
-
-            <div>
-                <label htmlFor="latitude">Latitude</label>
-                <Field name="latitude" component="input" type="text" />
-            </div>
-
-            <div>
-                <label htmlFor="longitude">Longitude</label>
-                <Field name="longitude" component="input" type="text" />
-            </div>
-
-            <div>
-                <label htmlFor="tags">Tags</label>
-                <Field name="tags" component="input" type="text" />
-            </div>
-
-            <div>
-                <label htmlFor="professionalExperience">Professional Experiences</label>
-                <Field name="professionalExperience" component="input" type="text" />
-            </div>
-
-            <div>
-                <label htmlFor="formation">Education</label>
-                <Field name="formation" component="input" type="text" />
-            </div>
-
-            <button type="submit">Submit</button>
-        </form>
+const renderField = ({ input, label, type, meta: { touched, error, warning } }) => (
+    <div>
+        <label>{label}</label>
+        <div>
+            <input {...input} type={type} />
+            {touched &&
+            ((error && <span>{error}</span>) ||
+            (warning && <span>{warning}</span>))}
+        </div>
     </div>
 )
 
+const CandidateForm = props => {
+    return (
+        <div> 
+            <h2> Add a new Candidate </h2>
+            <form onSubmit={props.handleSubmit}>
+                <div>
+                    <Field name="name" component={renderField} label="Full Name" type="text" />
+                </div>
+                <div>
+                    <Field name="email" component={renderField} label="Email" type="email" />
+                </div>
+
+                <div>
+                    <Field name="picture" component={renderField} label="Picture" type="text" />
+                </div>
+
+                <div>
+                    <Field name="birthDate" component={renderField} label="Birth Date" type="date" />
+                </div>
+
+                <div>
+                    <label>Gender</label>
+                    <label><Field name="gender" component={renderField} type="radio" value="male"/> Male</label>
+                    <label><Field name="gender" component={renderField} type="radio" value="female"/> Female</label>
+                </div>
+
+                <div>
+                    <Field name="phone" component={renderField} label="Telephone" type="number" />
+                </div>
+
+                <div>
+                    <Field name="address" component={renderField} label="Address" type="text" />
+                </div>
+
+                <div>
+                    <Field name="latitude" component={renderField} label="Latitude" type="text" />
+                </div>
+
+                <div>
+                    <Field name="longitude" component={renderField} label="Longitude" type="text" />
+                </div>
+
+                <div>
+                    <Field name="tags" component={renderField} label="Tags" type="text" />
+                </div>
+
+                <div>
+                    <Field name="professionalExperience" component={renderField} label="Professional Experiences" type="text" />
+                </div>
+
+                <div>
+                    <Field name="formation" component={renderField} label="Education" type="text" />
+                </div>
+
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    )
+}
+
 
 export default reduxForm({
-    form: 'candidate'
+    form: 'candidate',
+    validate
 })(CandidateForm)
