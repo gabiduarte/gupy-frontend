341fcbddfa00ea55b5bbd5cfa3e7094a9acd0a21
Gabrielle Saman <gabi.duarte@gmail.com>
2018-06-07 03:33:08 -0300

    Fix tests that were broken due to markup change

--- test/resume/resumeList.test.js
+++ test/resume/resumeList.test.js
@@ -19,7 +19,7 @@ describe('Resume List', () => {
     it('should show a message when there is no resumes to be shown', () => {
         const wrapper = mount(<ResumeList list={[]} fetchResumes={mockFetch}/>);
 
-        expect(wrapper.find('.resumes').text()).toEqual('No Resumes');
+        expect(wrapper.find('.resume').text()).toEqual('No Resumes');
     });
 
     it('should render a list of resumes when mounting the component with available list on props', () => {
@@ -29,7 +29,38 @@ describe('Resume List', () => {
             "gender": "male",
             "email": "rogerscampbell@aquasseur.com",
             "phone": "+55 11 (881) 501-2780",
-            "address": "862 Grove Street, Sussex, North Carolina, 9296"
+            "address": "862 Grove Street, Sussex, North Carolina, 9296",
+            "tags": [
+                "eiusmod",
+                "amet",
+                "commodo",
+                "sit",
+                "labore"
+              ],
+              "professionalExperiences": [
+                {
+                  "companyName": "ipsum",
+                  "role": "voluptate",
+                  "startDate": "2017-09-01T03:42:00 +03:00",
+                  "endDate": "2018-04-19T03:34:25 +03:00"
+                }
+              ],
+              "formations": [
+                {
+                  "institution": "sit",
+                  "course": "eu",
+                  "isConcluded": true,
+                  "startDate": "2014-04-23T07:48:08 +03:00",
+                  "endDate": "2015-01-03T02:38:36 +02:00"
+                },
+                {
+                  "institution": "anim",
+                  "course": "anim",
+                  "isConcluded": false,
+                  "startDate": "2015-04-26T07:58:56 +03:00",
+                  "endDate": "2014-12-25T06:28:08 +02:00"
+                }
+              ]
           },
           {
             "_id": "5678",
@@ -37,7 +68,38 @@ describe('Resume List', () => {
             "gender": "male",
             "email": "cochranmelton@aquasseur.com",
             "phone": "+55 11 (870) 551-2610",
-            "address": "714 Gilmore Court, Newry, New Jersey, 7758"
+            "address": "714 Gilmore Court, Newry, New Jersey, 7758",
+            "tags": [
+                "eiusmod",
+                "amet",
+                "commodo",
+                "sit",
+                "labore"
+              ],
+              "professionalExperiences": [
+                {
+                  "companyName": "ipsum",
+                  "role": "voluptate",
+                  "startDate": "2017-09-01T03:42:00 +03:00",
+                  "endDate": "2018-04-19T03:34:25 +03:00"
+                }
+              ],
+              "formations": [
+                {
+                  "institution": "sit",
+                  "course": "eu",
+                  "isConcluded": true,
+                  "startDate": "2014-04-23T07:48:08 +03:00",
+                  "endDate": "2015-01-03T02:38:36 +02:00"
+                },
+                {
+                  "institution": "anim",
+                  "course": "anim",
+                  "isConcluded": false,
+                  "startDate": "2015-04-26T07:58:56 +03:00",
+                  "endDate": "2014-12-25T06:28:08 +02:00"
+                }
+              ]
           }];
 
         const wrapper = mount(<ResumeList list={fakeResumeList} fetchResumes={mockFetch}/>);
